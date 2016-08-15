7e896276d4cce9475fb8e8f446b332325a49c6d2
wong2 <wonderfuly@gmail.com>
2016-08-15 16:57:26 +0800

    Use new qiniu sdk

--- app.py
+++ app.py
@@ -1,34 +1,31 @@
 #-*-coding:utf-8-*-
 
 from flask import Flask, render_template, request, jsonify
-from config import BUCKET_NAME, CALLBACK_URL, Q_DOMAIN, NEED_EXPIRE
-from q import qiniu
+from qiniu import Auth
+from config import BUCKET_NAME, CALLBACK_URL, Q_DOMAIN, EXPIRE_TIME, ACCESS_KEY, SECRET_KEY
 
+q = Auth(ACCESS_KEY, SECRET_KEY)
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    policy = qiniu.rs.PutPolicy(BUCKET_NAME)
-    policy.callbackBody = 'key=$(x:key)'
-    policy.callbackUrl = CALLBACK_URL
-    up_token = policy.token()
-
-    return render_template('index.html', up_token=up_token)
+    policy = {
+        'callbackUrl': CALLBACK_URL,
+        'callbackBody': 'key=$(key)',
+        'deleteAfterDays': EXPIRE_TIME,
+    }
+    token = q.upload_token(BUCKET_NAME, policy)
+    return render_template('index.html', token=token)
 
 
 @app.route('/callback', methods=['POST'])
 def upload_callback():
     key = request.form['key']
-    download_url = qiniu.rs.make_base_url(Q_DOMAIN, key)
-
-    if NEED_EXPIRE:
-        from cleaner import add_to_expire_queue
-        add_to_expire_queue(key)
-
+    base_url = 'http://%s/%s' % (Q_DOMAIN, key)
+    download_url = q.private_download_url(base_url)
     return jsonify(download_url=download_url)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- config.py
+++ config.py
@@ -8,14 +8,8 @@ Q_DOMAIN = '' # your qiniu domain
 
 CALLBACK_URL = '' # explained in http://docs.qiniu.com/api/v6/put.html#put-policy
 
+EXPIRE_TIME = 0 # file expire time in days (0 means no expire)
 
-# NOTE: following configs are only necessary if you need file expiration
-
-EXPIRE_TIME = 0 # file expire time in minutes (0 means no expire)
-NEED_EXPIRE = EXPIRE_TIME > 0
-
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
 
 try:
     from local_config import *
